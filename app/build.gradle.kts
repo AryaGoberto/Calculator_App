eeb3ee7acae6eb845bf7e48a0e9975a55e990e52
AryaGoberto <arguna2019@gmail.com>
2025-06-13 00:00:37 +0800

    lanjut di 21.45

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,4 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-}
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.4.1")}
---
+++ app/src/main/java/com/example/calculator_app/ui/theme/Calculator.kt
@@ -0,0 +1,51 @@
+package com.example.calculator_app.ui.theme
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.calculator_app.CalculatorState
+
+@Composable
+fun Calculator(
+    state: CalculatorState,
+    buttonSpacing: Dp = 8.dp,
+    modifier: Modifier = Modifier,
+) {
+    Box(modifier = modifier){
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter),
+            verticalArrangement = Arrangement.spacedBy(buttonSpacing)
+        ){
+            Text(
+                text = state.number1 + (state.operation?:"") + state.number2,
+                textAlign = TextAlign.End,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 32.dp),
+                fontWeight = FontWeight.Light,
+                fontSize = 80.sp,
+                color = Color.White,
+                maxLines = 2
+            )
+            //21.45
+            Row(){
+
+            }
+        }
+    }
+}
